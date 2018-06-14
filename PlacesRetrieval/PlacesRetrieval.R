85810a9937b9c4b09e6c8b244390d6fe9d76bfa2
MiguelGautier <miguelgautier11@gmail.com>
2018-06-13 22:27:17 -0400

    2nd commit

--- PlacesRetrieval/PlacesRetrieval.R
+++ PlacesRetrieval/PlacesRetrieval.R
@@ -12,20 +12,15 @@ Name <- "PlacesRetrieval"
 
 ## This script aims to retrieve from Google Places API places 
 ## that comply with the specified location, radius, keyword, type, and keyword.
-## The default configuration will set the radius in 50 000 meters (maximum allowed),
-## and for the location a table will be provided, with the coordinates necessary to 
-## search through all the Dominican territory. 
-## Type and keyword can be changed in the script. 
-## In a updated version the script will ask the user to provide these variables.
-## Also will allow to choose a specific province or region within the DR to search. 
+
 
 #Packages 
 
     ##XML package
-    if(!("XML" %in% rownames(installed.packages()))){
-      install.packages("XML")
+    if(!("xml2" %in% rownames(installed.packages()))){
+      install.packages("xml2")
     }
-    library("XML")
+    library("xml2" )
   
     ##data.table Package
     if(!("data.table" %in% rownames(installed.packages()))){
@@ -38,52 +33,103 @@ Name <- "PlacesRetrieval"
 #Variables
 
     ## Search radius
-    radius <- 50000   
+    if (!exists("radius")){
+      radius <- 50000   
+    }
     radius <- file.path("radius",radius,fsep = "=")
     
+    ## API key 
+    if (exists("key")){
+      key <- file.path("key",key,fsep = "=")
+    } else print("Key must be specified")
+    
     ## Keyword that identifies the place searched. 
-    keyword <- "popular"
-    keyword <- file.path("keyword",keyword,fsep = "=")
+    if (exists("keyword")){
+      keyword <- file.path("keyword",keyword,fsep = "=")  
+    } 
+    
     
     ## Type of place searched. To see available types 
     ## typesvalues <- read.table("Type.txt",header = FALSE)
-    type <- "bank"
-    type <- file.path("type",type,fsep = "=")
+    if (exists("type")){
+      type <- file.path("type",type,fsep = "=")
+    }
     
-    ## API key 
-    key <- "AIzaSyBaUM6Q8PciqtZQSkgJ_Oa1Pdwq_xJCzcg"
-    key <- file.path("key",key,fsep = "=")
-    
+   
     ## Group variables in a single string
-    variables <- file.path(radius,type,keyword,key,fsep = "&")
-    
+    if (exists("keyword") && exists("type")){
+      variables <- file.path(radius,type,keyword,key,fsep = "&")
+    }
+    if (!exists("keyword") && exists("type")){
+      variables <- file.path(radius,type,key,fsep = "&")
+    }
+    if (exists("keyword") && !exists("type")){
+      variables <- file.path(radius,keyword,key,fsep = "&")
+    }
     
     ## Search points.
     ##txt file that contains coordinates of search points around the DR
-    locationstable <- data.frame(read.table("Coordenadas.txt", stringsAsFactors = FALSE))
+    if(!exists(location)){
+      locationstable <- data.frame(read.table("Coordenadas.txt", stringsAsFactors = FALSE))
+    } else locationstable <- data.frame(location)
+    
     names(locationstable) <- c("Coordinates") 
     lim <- nrow(locationstable)
     
     ## Function to generate the correspondent url string for each coordinate
-    url <- function(i){
+    url1 <- function(i){
       location <- file.path("location",locationstable[i,1],fsep = "=")
       return(file.path(file.path("https://maps.googleapis.com/maps/api/place/radarsearch/xml?",
                 location,fsep = ""),variables,fsep = "&"))
     }
     
     ## Inserting url values and an ID to each coordinate as additional columns
-    locationstable$URL<- url(seq(1,lim))
+    locationstable$URL<- url1(seq(1,lim))
     locationstable$ID <- seq(1,lim) 
   
     ## Loop Coordinates to get locations 
+  
     coordinates <- setNames(data.frame(matrix(ncol = 2, nrow = 0)), c("lat", "lng"))
     
     for (i in 1:lim) {
-      tree <- read_xml(locationstable$URL[i])
-      latitude<-setNames(as.data.frame(xml_text(xml_find_all(tree,"//lat"))),"lat")
-      longitude<-setNames(as.data.frame(xml_text(xml_find_all(tree,"//lng"))),"lng")
-      coordinates <- rbind(coordinates,cbind.data.frame(latitude,longitude))
+      radar <- read_xml(locationstable$URL[i])
+      latitude<-setNames(as.data.frame(xml_text(xml_find_all(radar,"//lat"))),"lat")
+      longitude<-setNames(as.data.frame(xml_text(xml_find_all(radar,"//lng"))),"lng")
+      placeid <- setNames(as.data.frame(xml_text(xml_find_all(radar,"//place_id"))),"Place ID")
+      coordinates <- rbind(coordinates,cbind.data.frame(latitude,longitude,placeid))
+      
       print(locationstable$ID[i])
     }
+    
+    ## Function to generate the correspondent url string for each placeid
+    url2 <- function(i){
+      PlaceId <- file.path("placeid",coordinates$`Place ID`[i],fsep = "=")
+      return(file.path(file.path("https://maps.googleapis.com/maps/api/place/details/xml?",
+                                 PlaceId,fsep = ""),key,fsep = "&"))
+    }
+    ## Inserting url values and an ID to each coordinate as additional columns
+    P <- nrow(coordinates)
+    placesidURL<- url2(seq(1,P))
+    placesdetails <- setNames(data.frame(matrix(ncol = 3, nrow = 0)), c("Name", "Phone", "Address"))
+    
+    ## Loop Places ID to get details 
+    
+    for (i in 1:P) {
+      details <- read_xml(placesidURL[i])
+      
+      placename<-xml_text(xml_find_all(details,"//name"))
+      placeaddress<- xml_text(xml_find_all(details,"//formatted_address"))
+      placephone<-if(identical(xml_text(xml_find_all(details,"//formatted_phone_number"))
+                     ,character(0))){
+        "ND"
+      } else xml_text(xml_find_all(details,"//formatted_phone_number"))
+      
+      placesdetails <- rbind(placesdetails,cbind(placename,placephone, placeaddress))
+      print(i)
+      
+  }
+ 
     ## Write coordinates into a csv file
-    write.csv(coordinates,file = "Coordenadas")
+    write.csv(coordinates,file = "Coordenadas.csv")
+    write.csv(placesdetails, file = "Detalles.csv")
+    remove(key,keyword,type,variables)
